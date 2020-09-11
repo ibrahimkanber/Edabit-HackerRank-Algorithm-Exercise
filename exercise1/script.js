1edca163a71751397415593fe76ae3ae333ffd1c
ibrahimKanber <ikanber45@gmail.com>
2020-09-11 22:41:56 +0200

    redundant

--- exercise1/script.js
+++ exercise1/script.js
@@ -1,4 +1,4 @@
-/*
+/*Q:1
 ////How Much is True?
 //// Create a function which returns the number of true values there are in an array.
 //Answer1
@@ -10,19 +10,28 @@ function countTrue(arr) {
     return (count)
 }
 countTrue([true,false,false])
-*/
+
 //Answer2
 function countTrue(arr) {
     return arr.filter(item=>item===true).length
      
  }
 countTrue([true,false,false])
+*/
+
 
 ///--------------------------///
+/*
+//Q:2
+//A Redundant Function
+// Write a function redundant that takes in a string str and returns a function that returns str.
 
-
-function countTrue(arr) {
-   return arr.filter(item=>item===true).length
-    
+//Answer
+function redundant(str) {
+    return function(){ 
+          return str   
+    }   
 }
-countTrue([true,false,false])
+
+*/
+
