1ee4ffe5072cbf4564bec212f704ab9de1d2d572
ibrahimKanber <ikanber45@gmail.com>
2020-09-11 23:29:29 +0200

    exercise

--- exercise1/script.js
+++ exercise1/script.js
@@ -35,3 +35,52 @@ function redundant(str) {
 
 */
 
+/*
+///Q:3
+// Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.simpleArraySum has the following parameter(s): ar: an array of integers
+
+// function simpleArraySum(ar) {
+//    
+//     return ar.reduce(((a,b)=>a+b),0)
+// }
+*/
+/*
+//Q:4
+// Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+
+
+// The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
+
+// If a[i] > b[i], then Alice is awarded 1 point.
+// If a[i] < b[i], then Bob is awarded 1 point.
+// If a[i] = b[i], then neither person receives a point.
+// Comparison points is the total points a person earned.
+
+// Given a and b, determine their respective comparison points.
+
+//Answer:
+function compareTriplets(a, b) {
+    let alice=0;
+    let bob=0;
+    for (let i=0;i<a.length;i++){
+        if (a[i]>b[i]){
+            alice++
+        }else if(a[i]<b[i]){
+            bob++
+        }else {
+            continue
+        }
+    
+    }
+
+    console.log(alice,bob)
+    
+}
+
+*/
+
+
+
+
+
+
